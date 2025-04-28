ace8c41dc001e8e3c9e30a21c1607d6f2760cb4f
Ilsa <irehmat123@gmail.com>
2025-04-28 09:48:18 -0400

    added psuedocode in activity class for the method getSPeed

---
+++ Activity.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Activity here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Activity
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Activity
+     */
+    public Activity()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    // public double getSpeed(){
+        // Pseudocode
+           // if duaration > 0 then
+              // RETURN distance divided by duration
+           // Else
+              // RETURN 0
+           // END if
+        // END method
+    // }
+
+    
+}
