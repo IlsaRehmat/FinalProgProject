5c0f49425ece7ac95708c9c56beb423a5583d550
saad <saadvhere007@gmail.com>
2025-04-28 20:03:21 -0400

    pseudocodde for poweredActitvity

--- PoweredActivity.java
+++ PoweredActivity.java
@@ -18,6 +18,36 @@ public class PoweredActivity extends Activity
         // initialise instance variables
         x = 0;
     }
+    
+    // pseudocode for Powered activity
+    
+    // equipment: String 
+    
+    //  CONSTRUCTOR PoweredActivity(mode: String, distance: float, duration:
+    //  SET self.mode = mode
+    //  SET self.distance = distance
+    //    SET self.duration = duration
+     //   SET self.equipment = equipment
+    //END CONSTRUCTOR
+    
+    //METHOD getSpeed() RETURNS float
+    //    IF duration > 0 THEN
+    //        RETURN distance / duration
+    
+    //ELSE
+    //        PRINT "Invalid duration: cannot compute speed."
+    //        RETURN 0
+    //    END IF
+    //END METHOD
+
+    //METHOD displayDetails()
+    //    PRINT "Powered Activity: " + mode
+    //   PRINT "Equipment Used: " + equipment
+    //    PRINT "Distance: " + distance + " km"
+    //    PRINT "Duration: " + duration + " hours"
+    //    PRINT "Speed: " + getSpeed() + " km/h"
+    // END METHOD
+
 
     /**
      * An example of a method - replace this comment with your own
