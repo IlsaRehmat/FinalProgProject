4230be19416a6c2db12fe1976e9ec981cf370763
Nick <Nicky.amaral@icloud.com>
2025-05-08 09:00:05 -0400

    coded Regular activity fixing the compile issue

--- RegularActivity.java
+++ RegularActivity.java
@@ -6,7 +6,24 @@ import java.util.Date;
  * @version (a version number or a date)
  */
 public class RegularActivity extends Activity {
-    
+      public RegularActivity(double distance, double duration, Date date, ModeOfTransport mode) {
+        super(distance, duration, date, mode);
+    }
+
+    @Override
+    public double getCaloriesBurned() {
+        switch (mode) {
+            case WALKING: return distance * 55;
+            case RUNNING: return distance * 100;
+            case SWIMMING: return distance * 120;
+            default: return distance * 50;
+        }
+    }
+
+    @Override
+    public String getSummary() {
+        return mode + ": " + distance + " km in " + duration + " hrs on " + date;
+    }
 
     // this class is for excercises that do not require any equipment. 
     // Ex: swimming, running and walking
